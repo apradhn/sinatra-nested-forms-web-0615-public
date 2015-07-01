d02609ccf12759cadac6a16b1d2e0e8ed1a8adb2
apradhn <abhijit.pradhan6@gmail.com>
2015-07-01 12:52:58 -0400

    all done

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :'root'
+    end
+
+    get '/new' do 
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do 
+      @name = params[:pirate][:name]
+      @height = params[:pirate][:height]
+      @weight = params[:pirate][:weight]
+      @ships = params[:pirate][:ships].collect{|num, data| data}
+      erb :'pirates/show'
+    end
 
   end
 end
